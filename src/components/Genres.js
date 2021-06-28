41d0bc8f734e8fd74c98ee5dfc59721a7e46116c
anle99 <lehoanagn17121999@gmail.com>
2021-06-28 12:19:55 +0700

    commit enteraiment-hub

---
+++ src/components/Genres.js
@@ -0,0 +1,71 @@
+import {useEffect} from "react";
+import axios from "axios"
+import { Chip } from "@material-ui/core";
+
+
+const Genres = ({
+    selectedGenres,
+    setSelectedGenres,
+    genres,
+    setGenres,
+    type,
+    setPage,
+}) => {
+
+    const handleAdd = (genre) => {
+        setSelectedGenres([...selectedGenres, genre]);
+        setGenres(genres.filter((g) => g.id !== genre.id));
+        setPage(1);
+    };
+    const handleRemove = (genre) => {
+        setSelectedGenres(
+          selectedGenres.filter((selected) => selected.id !== genre.id)
+        );
+        setGenres([...genres, genre]);
+        setPage(1);
+    };
+
+    const fetchGenres = async () => {
+       const { data } = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+       );
+       
+       setGenres(data.genres);
+    };
+    console.log(genres);
+    useEffect(() => {
+        fetchGenres();
+        return ()=>{
+            setGenres({});
+        }
+    }, []);
+    // eslint-disable-next-line
+    return (
+        <div style={{ padding: "6px 0" }} >
+            { selectedGenres 
+            && selectedGenres.map((genre) => (
+                <Chip 
+                    style={{ margin: 2 }}
+                    label={genre.name}
+                    key={genre.id}
+                    clickable
+                    size="small"
+                    onDelete={() => handleRemove(genres)}
+                />
+            )) }
+
+            { genres 
+            && genres.map((genre) => (
+                <Chip 
+                    style={{ margin: 2 }}
+                    label={genre.name}
+                    key={genre.id}
+                    clickable
+                    size="small"
+                    onClick={() => handleAdd(genres)}
+                />
+            )) }
+        </div>
+    )
+}
+
+export default Genres
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+const Header = () =>{
+    return(
+        <span onClick={()=>window.scroll(0,0)} className="header">🎬 Entertaiment Hub 📽 </span>
+    )
+}
+
+export default Header
